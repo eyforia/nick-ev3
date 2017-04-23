790020e3e74046501407c8bf1d060ae2f7b40160
Andrei Karneyenka <eyforia@gmail.com>
2017-04-23 11:17:29 -0400

    LED fading in infinite loop

--- main.js
+++ main.js
@@ -3,21 +3,24 @@ var ev3dev = require('ev3dev-lang');
 
 console.log('fading LEDs from green to red...');
 
-for (var pct = 0; pct < 100; pct += 1) {
-    var brightnessVal = (pct / 100);
-    var invertedBrightnessVal = 1 - brightnessVal;
 
-    ev3dev.Ev3Leds.left.setColor([ brightnessVal, invertedBrightnessVal ]);
-    ev3dev.Ev3Leds.right.setColor([ brightnessVal, invertedBrightnessVal ]);
+while(true) {
+    for (var pct = 0; pct < 100; pct += 1) {
+        var brightnessVal = (pct / 100);
+        var invertedBrightnessVal = 1 - brightnessVal;
 
-    if(pct % 10 == 0)
-        console.log(pct + '%');
+        ev3dev.Ev3Leds.left.setColor([brightnessVal, invertedBrightnessVal]);
+        ev3dev.Ev3Leds.right.setColor([brightnessVal, invertedBrightnessVal]);
 
-    {   //Hack to sleep for time
-        //    SHOULD NOT BE USED IN PRODUCTION CODE
-        var start = new Date().getTime();
-        while(new Date().getTime() < start + 100) {
-            ;
+        if (pct % 10 == 0)
+            console.log(pct + '%');
+
+        {   //Hack to sleep for time
+            //    SHOULD NOT BE USED IN PRODUCTION CODE
+            var start = new Date().getTime();
+            while (new Date().getTime() < start + 100) {
+                ;
+            }
         }
     }
 }
