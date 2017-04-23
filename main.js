34f074c05389548cef0beee7c3265e097fa4f331
Andrei Karneyenka <eyforia@gmail.com>
2017-04-23 11:20:06 -0400

    alternating LED color

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ var ev3dev = require('ev3dev-lang');
 
 console.log('fading LEDs from green to red...');
 
-
 while(true) {
     for (var pct = 0; pct < 100; pct += 1) {
         var brightnessVal = (pct / 100);
         var invertedBrightnessVal = 1 - brightnessVal;
 
         ev3dev.Ev3Leds.left.setColor([brightnessVal, invertedBrightnessVal]);
-        ev3dev.Ev3Leds.right.setColor([brightnessVal, invertedBrightnessVal]);
+        ev3dev.Ev3Leds.right.setColor([invertedBrightnessVal, brightnessVal]);
 
         if (pct % 10 == 0)
             console.log(pct + '%');
