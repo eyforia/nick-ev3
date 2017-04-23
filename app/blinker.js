643998846bacfcf5732c34725c69cb72ebe693f0
Andrei Karneyenka <eyforia@gmail.com>
2017-04-23 11:37:39 -0400

    moved execution logic to modules

---
+++ app/blinker.js
@@ -0,0 +1,33 @@
+var ev3dev = require('ev3dev-lang');
+
+
+blinker = function () {
+    console.log('fading LEDs from green to red...');
+
+    for (var pct = 0; pct < 100; pct += 1) {
+        var brightnessVal = (pct / 100);
+        var invertedBrightnessVal = 1 - brightnessVal;
+
+        ev3dev.Ev3Leds.left.setColor([brightnessVal, invertedBrightnessVal]);
+        ev3dev.Ev3Leds.right.setColor([invertedBrightnessVal, brightnessVal]);
+
+        if (pct % 10 == 0)
+            console.log(pct + '%');
+
+        {   //Hack to sleep for time
+            //    SHOULD NOT BE USED IN PRODUCTION CODE
+            var start = new Date().getTime();
+            while (new Date().getTime() < start + 50) {
+                ;
+            }
+        }
+    }
+
+    console.log('done');
+
+    ev3dev.Ev3Leds.left.allOff();
+    ev3dev.Ev3Leds.right.allOff();
+
+};
+
+module.exports = blinker;
--- main.js
+++ main.js
@@ -1,28 +1,5 @@
 #!/usr/bin/env node
-var ev3dev = require('ev3dev-lang');
 
-console.log('fading LEDs from green to red...');
+var blinker = require('./app/blinker');
 
-    for (var pct = 0; pct < 100; pct += 1) {
-        var brightnessVal = (pct / 100);
-        var invertedBrightnessVal = 1 - brightnessVal;
-
-        ev3dev.Ev3Leds.left.setColor([brightnessVal, invertedBrightnessVal]);
-        ev3dev.Ev3Leds.right.setColor([invertedBrightnessVal, brightnessVal]);
-
-        if (pct % 10 == 0)
-            console.log(pct + '%');
-
-        {   //Hack to sleep for time
-            //    SHOULD NOT BE USED IN PRODUCTION CODE
-            var start = new Date().getTime();
-            while (new Date().getTime() < start + 50) {
-                ;
-            }
-        }
-    }
-
-console.log('done');
-
-ev3dev.Ev3Leds.left.allOff();
-ev3dev.Ev3Leds.right.allOff();
+// blinker();
